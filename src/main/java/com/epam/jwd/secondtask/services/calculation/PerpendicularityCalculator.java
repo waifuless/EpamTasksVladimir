e3020a2c8e8ae4da635f1030c967f34ffdc8f3ce
Vova <vova.prusenok@gmail.com>
2021-08-12 01:08:19 +0300

    put the code in the parallelism functions in PerpendicularityCalculator

--- src/main/java/com/epam/jwd/secondtask/services/calculation/PerpendicularityCalculator.java
+++ src/main/java/com/epam/jwd/secondtask/services/calculation/PerpendicularityCalculator.java
@@ -8,30 +8,30 @@ import java.math.BigDecimal;
 public class PerpendicularityCalculator {
 
     public static boolean isPlanePerpendicularOxy(Plane plane) {
-        if (plane.getCoefficientB().compareTo(plane.getCoefficientC()) == 0
-                && plane.getCoefficientB().compareTo(BigDecimal.ZERO) == 0) {
-            return true;
-        }
-        return plane.getCoefficientA().compareTo(plane.getCoefficientC()) == 0
-                && plane.getCoefficientA().compareTo(BigDecimal.ZERO) == 0;
+        return isPlaneParallelOxz(plane) || isPlaneParallelOyz(plane);
     }
 
     public static boolean isPlanePerpendicularOxz(Plane plane) {
-        if (plane.getCoefficientB().compareTo(plane.getCoefficientA()) == 0
-                && plane.getCoefficientB().compareTo(BigDecimal.ZERO) == 0) {
-            return true;
-        }
-        return plane.getCoefficientB().compareTo(plane.getCoefficientC()) == 0
-                && plane.getCoefficientB().compareTo(BigDecimal.ZERO) == 0;
+        return isPlaneParallelOxy(plane) || isPlaneParallelOyz(plane);
     }
 
     public static boolean isPlanePerpendicularOyz(Plane plane) {
-        if (plane.getCoefficientB().compareTo(plane.getCoefficientA()) == 0
-                && plane.getCoefficientB().compareTo(BigDecimal.ZERO) == 0) {
-            return true;
-        }
+        return isPlaneParallelOxy(plane) || isPlaneParallelOxz(plane);
+    }
+
+    private static boolean isPlaneParallelOxy(Plane plane){
+        return plane.getCoefficientB().compareTo(plane.getCoefficientA()) == 0
+                && plane.getCoefficientB().compareTo(BigDecimal.ZERO) == 0;
+    }
+
+    private static boolean isPlaneParallelOxz(Plane plane){
         return plane.getCoefficientA().compareTo(plane.getCoefficientC()) == 0
                 && plane.getCoefficientA().compareTo(BigDecimal.ZERO) == 0;
+    }
+
+    private static boolean isPlaneParallelOyz(Plane plane){
+        return plane.getCoefficientB().compareTo(plane.getCoefficientC()) == 0
+                && plane.getCoefficientB().compareTo(BigDecimal.ZERO) == 0;
     }
 
 }
