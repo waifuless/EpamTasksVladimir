e22394462f10298870890f1fa614496b90b4c633
Vova <vova.prusenok@gmail.com>
2021-08-16 15:49:54 +0300

    add tests to PlaneRegistrar

--- src/test/java/com/epam/jwd/secondtask/model/PlaneRegistrarTest.java
+++ src/test/java/com/epam/jwd/secondtask/model/PlaneRegistrarTest.java
@@ -1,5 +1,7 @@
 package com.epam.jwd.secondtask.model;
 
+import com.epam.jwd.secondtask.exception.PlaneIsInvalidException;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.math.BigDecimal;
@@ -33,5 +35,23 @@ public class PlaneRegistrarTest {
                 BigDecimal.valueOf(65.9052).setScale(4, RoundingMode.HALF_UP));
         assertEquals(registrar.getAngleWithOyz().setScale(4, RoundingMode.HALF_UP),
                 BigDecimal.valueOf(35.2644).setScale(4, RoundingMode.HALF_UP));
+    }
+
+    @Test
+    public void testIsPlaneValid(){
+        Plane plane = Plane.of(0,1,0,0);
+        plane.setCoefficientB(BigDecimal.ZERO); // now should be invalid
+        PlaneRegistrar registrar = new PlaneRegistrar("lola",plane);
+        Assert.assertFalse(registrar.isPlaneValid());
+        plane.setCoefficientB(BigDecimal.TEN);
+        Assert.assertTrue(registrar.isPlaneValid());
+    }
+
+    @Test(expectedExceptions = PlaneIsInvalidException.class)
+    public void testException_getAngle_when_PlaneIsInvalid(){
+        Plane plane = Plane.of(0,1,0,0);
+        plane.setCoefficientB(BigDecimal.valueOf(0)); // now its invalid
+        PlaneRegistrar registrar = new PlaneRegistrar("lola",plane);
+        registrar.getAngleWithOxy();
     }
 }
