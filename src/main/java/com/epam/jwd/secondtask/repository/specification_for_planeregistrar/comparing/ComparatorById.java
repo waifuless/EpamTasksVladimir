e9e3fd2a75cd787692cd7eb4ccb798cf42dbbf6d
Vova <vova.prusenok@gmail.com>
2021-08-16 17:26:37 +0300

    repair ComparatorById and make some sort test

--- src/main/java/com/epam/jwd/secondtask/repository/specification_for_planeregistrar/comparing/ComparatorById.java
+++ src/main/java/com/epam/jwd/secondtask/repository/specification_for_planeregistrar/comparing/ComparatorById.java
@@ -11,7 +11,7 @@ public class ComparatorById<T extends PlaneRegistrar> implements Comparator<T> {
 
     @Override
     public int compare(T elem1, T elem2) {
-        return Long.compare(elem2.getId(), elem1.getId());
+        return Long.compare(elem1.getId(), elem2.getId());
     }
 
 }
---
+++ src/test/java/com/epam/jwd/secondtask/repository/RepositorySortTest.java
@@ -0,0 +1,49 @@
+package com.epam.jwd.secondtask.repository;
+
+
+import com.epam.jwd.secondtask.model.Plane;
+import com.epam.jwd.secondtask.model.PlaneRegistrar;
+import com.epam.jwd.secondtask.repository.specification_for_planeregistrar.comparing.ComparatorFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.testng.Assert.*;
+
+public class RepositorySortTest {
+
+    private final static RepositoryOnList<PlaneRegistrar> repository = new RepositoryOnList<>();
+
+
+    @BeforeMethod
+    public void init_Repository() {
+        repository.deleteAll();
+        Random random = new Random();
+        for (int i = 1; i <= 10; i++) {
+            repository.save(new PlaneRegistrar(String.format("%d", i),
+                    Plane.of(random.nextDouble()*20-7, random.nextDouble()*20-7,
+                            random.nextDouble()*20-7, random.nextDouble()*20-7)));
+        }
+    }
+
+    @Test
+    public void testSortById(){
+        repository.sort(ComparatorFactory.getComparator(ComparatorFactory.CompareParameter.ID));
+        List<PlaneRegistrar> list = repository.findAll();
+        for (int i = 0; i < list.size() - 1; i++) {
+            assertTrue(list.get(i).getId()<list.get(i+1).getId());
+        }
+    }
+
+    @Test
+    public void testSortById_Reversed(){
+        repository.sort(ComparatorFactory.getComparator(ComparatorFactory.CompareParameter.ID).reversed());
+        List<PlaneRegistrar> list = repository.findAll();
+        for (int i = 0; i < list.size() - 1; i++) {
+            assertTrue(list.get(i).getId()>list.get(i+1).getId());
+        }
+    }
+
+}
